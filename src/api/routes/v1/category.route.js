4438b62ae122724ce3abcfa40f220a11ac7efcfc
hualocson <85344481+hualocson@users.noreply.github.com>
2023-04-21 11:06:55 +0700

    #10 modify category route

--- src/api/routes/v1/category.route.js
+++ src/api/routes/v1/category.route.js
@@ -11,30 +11,39 @@ router.use('/products', productRoute)
 // '/categories/variations/'
 router.use('/variations', variationRoute)
 
+// #region /categories
+
+// #region get
+
 // [GET] '/categories/'
 router.get('/', categoryController.getListCategory)
 
 // [GET] '/categories/:id/products'
 router.get('/:id/products', categoryController.getListProductByCategoryId)
 
+// [GET] '/categories/:id/variations'
+router.get('/:id/variations', categoryController.getListVariationByCategoryId)
+
 // [GET] '/categories/:id'
 router.get('/:id', categoryController.getCategoryById)
+// #endregion get
 
-// variations
+// #region post
+
 // [POST] '/categories/:id/variations'
 router.post('/:id/variations', categoryController.createVariationByCategoryId)
 
-// [GET] '/categories/:id/variations'
-router.get('/:id/variations', categoryController.getListVariationByCategoryId)
-// end variations
-
 // [POST] '/categories/'
 router.post('/', categoryController.createCategory)
+
+// #endregion post
 
 // [PATCH] '/categories/:id'
 router.patch('/:id', categoryController.updateCategory)
 
 // [DELETE] '/categories/:id'
 router.delete('/:id', categoryController.deleteCategory)
+
+// #endregion /categories
 
 export default router
